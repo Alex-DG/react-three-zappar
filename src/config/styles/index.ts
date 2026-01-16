3a69babc2b33140db6da931df97987c61c46fd5c
Alex Di Guida <alexandre.diguida@gmail.com>
2026-01-16 13:26:50 +0900

    Initial commit with Openssl legacy provider fix

---
+++ src/config/styles/index.ts
@@ -0,0 +1,39 @@
+import * as styled from 'styled-components'
+import { Color } from './colors'
+
+const fullStyling = styled.css`
+  width: 100%;
+  height: 100%;
+`
+
+const GlobalStyling = styled.createGlobalStyle`
+  html,
+  body,
+  #root {
+    margin: 0;
+    padding: 0;
+
+    ${fullStyling};
+
+    font-family: Arial, Helvetica, Arial, sans-serif;
+  }
+
+  .app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    ${fullStyling};
+  }
+
+  .link {
+    font-size: bold;
+    color: ${Color.SECONDARY};
+
+    :hover {
+      color: #000;
+    }
+  }
+`
+export default GlobalStyling
